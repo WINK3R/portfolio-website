cf93a6e697a05a962b219353c86f2ad41b468f4c
ludelanier <lucas.delanier@etu.uca.fr>
2024-05-08 04:32:42 +0200

    images

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), svgLoader()],
-  base: '/',
+  base: 'https://codefirst.iut.uca.fr/',
 })
