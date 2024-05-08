d964b50059928c9ff95e08df39a1e1b3cccd4068
ludelanier <lucas.delanier@etu.uca.fr>
2024-05-08 04:03:10 +0200

    images

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,4 @@ import svgLoader from 'vite-svg-loader'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), svgLoader()],
-  base: 'containers/lucasdelanier-portfolio'
 })
